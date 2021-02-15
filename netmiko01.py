4d2931da8de38bf7e4c5fcf1ca6f8eff79830e4b
ZeroHX <61070023@kmitl.ac.th>
2021-02-15 17:22:42 +0000

    Add config test script ACL/SSH

--- netmiko01.py
+++ netmiko01.py
@@ -3,6 +3,7 @@ device_ip = "172.31.174."
 username = "admin"
 password = "cisco"
 
+## Assign loopback IP
 # for ip_last in range(1, 10):
 #     device_ip = device_ip + str(ip_last)
 #     print(device_ip)
@@ -19,12 +20,70 @@ password = "cisco"
 #         result = ssh.send_command('sh ip int br')
 #         result = ssh.send_command('wri')
 #         print(result)
-
-        
+    
 #     device_ip = device_ip[:-1]
 
+## Assign IP
+# for ip_last in range(4,10):
+#     device_ip = device_ip + str(ip_last)
+#     print(device_ip)
+#     if ip_last != 8:
+#         print(device_ip)
+#         device_para = {'device_type': 'cisco_ios',
+#                     'ip': device_ip,
+#                     'username': username,
+#                     'password': password
+#                     }
+#         with ConnectHandler(**device_para) as ssh:
+#             if ip_last < 8:
+#                 router_file = 'config_ip/r%d.txt'%(ip_last-3)
+#             else:
+#                 router_file = 'config_ip/r%d.txt'%(ip_last-4)
+#             result = ssh.send_config_from_file(router_file)
+#             result = ssh.send_command('sh ip int br')
+#             print(result)
+#     device_ip = device_ip[:-1]
+
+
+## Add ACL
+# for ip_last in range(4,10):
+#     device_ip = device_ip + str(ip_last)
+#     print(device_ip)
+#     if ip_last != 8:
+#         print(device_ip)
+#         device_para = {'device_type': 'cisco_ios',
+#                     'ip': device_ip,
+#                     'username': username,
+#                     'password': password
+#                     }
+#         with ConnectHandler(**device_para) as ssh:
+#             router_file = 'config_acl.txt'
+#             result = ssh.send_config_from_file(router_file)
+#             print(result)
+#             ssh.save_config()
+#     device_ip = device_ip[:-1]
+
+## Add SSH
+# for ip_last in range(1,10):
+#     if ip_last not in [2,3,8]:
+#         device_ip = device_ip + str(ip_last)
+#         print(device_ip)
+#         device_para = {'device_type': 'cisco_ios',
+#                     'ip': device_ip,
+#                     'username': username,
+#                     'password': password
+#                     }
+#         with ConnectHandler(**device_para) as ssh:
+#             router_file = 'config_ssh.txt'
+#             result = ssh.send_config_from_file(router_file)
+#             ssh.save_config()
+#             print(result)
+#         device_ip = device_ip[:-1]
+
+## Add OSPF
 for ip_last in range(4,10):
     device_ip = device_ip + str(ip_last)
+    print(device_ip)
     if ip_last != 8:
         print(device_ip)
         device_para = {'device_type': 'cisco_ios',
@@ -41,3 +100,6 @@ for ip_last in range(4,10):
             result = ssh.send_command('sh ip int br')
             print(result)
     device_ip = device_ip[:-1]
+
+
+    
